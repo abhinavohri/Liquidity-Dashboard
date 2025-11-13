739c77669b0d9bc336e0274c602de3d6b9377639
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-11-13 15:23:52 +0530

    Setup sql over http with ponder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,35 +2,42 @@ import AnalyticsCard from "./components/AnalyticsCard";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LiquidationsTable from "./components/LiquidationsTable";
+import { PonderProvider } from "@ponder/react";
+import { client } from "../lib/ponder";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
+
+const queryClient = new QueryClient(); 
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Liquidations</h1>
-        <Box sx={{ flexGrow: 1, padding: 2 }}>
-          <Grid container spacing={2}>
-            <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-              <AnalyticsCard header='# of Liquidations' value="717" />
-            </Grid>
+    <PonderProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <h1>Liquidations</h1>
+          <Box sx={{ flexGrow: 1, padding: 2 }}>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <AnalyticsCard header='# of Liquidations' value="717" />
+              </Grid>
 
-            <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-              <AnalyticsCard header='Collateral Liquidated' value="6.79" />
-            </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <AnalyticsCard header='Collateral Liquidated' value="6.79" />
+              </Grid>
 
-            <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-              <AnalyticsCard header='Debt Repaid' value="6.17" />
-            </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <AnalyticsCard header='Debt Repaid' value="6.17" />
+              </Grid>
 
-            <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-              <AnalyticsCard header='Nº of liquidators' value="43" />
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <AnalyticsCard header='Nº of liquidators' value="43" />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-        <LiquidationsTable />
-      </div>
-    </>
+          </Box>
+          <LiquidationsTable />
+        </div>
+      </QueryClientProvider>
+    </PonderProvider>
   )
 }
 
--- liquidity-indexer/src/api/index.ts
+++ liquidity-indexer/src/api/index.ts
@@ -1,9 +1,10 @@
+import { db } from "ponder:api";
+import schema from "ponder:schema";
 import { Hono } from "hono";
+import { client } from "ponder"; 
  
 const app = new Hono();
  
-app.get("/hello", (c) => {
-  return c.text("Hello, world!");
-});
+app.use("/sql/*", client({ db, schema })); 
  
 export default app;
